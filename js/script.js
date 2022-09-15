9a54564ee108fb39d8ab2a69f371e8a478422bc7
Madie Few <mfew.business@gmail.com>
2022-09-15 15:39:06 -0500

    Troubleshooting try 1

---
+++ js/script.js
@@ -0,0 +1,43 @@
+//1 The unordered list where the player’s guessed letters will appear.
+
+const guessedLetter = document.querySelector("ul") {
+
+};
+
+//2 The button with the text “Guess!” in it.
+
+const buttonGuess = document.addEventListener("click", function () {
+
+});
+
+//3 The text input where the player will guess a letter.
+
+const input = prompt("Type one letter:");
+
+//4 The empty paragraph where the word in progress will appear.
+
+const buildWord = document.querySelector(".word-in-progress") {
+
+}
+
+//5 The paragraph where the remaining guesses will display.
+
+const numGuessRem = document.querySelector(".remaining"){
+
+}
+
+//6 The span inside the paragraph where the remaining guesses will display.
+
+const spanGuessRem = document.querySelector(".guess-form"){
+
+}
+
+//7 The empty paragraph where messages will appear when the player guesses a letter.
+
+//?
+
+//8 The hidden button that will appear prompting the player to play again.
+
+const buttonHide = document.addEventListener("click", function () {
+
+}
