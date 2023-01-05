21e8adf42497a9398dc892d223329b5b5c53bbc4
Madie Few <mfew.business@gmail.com>
2023-01-05 16:44:42 -0600

    Aligning details with solution

--- js/script.js
+++ js/script.js
@@ -4,50 +4,51 @@ const guessedLettersElement = document.querySelector(".guessed-letters");
 
 //2b The button with the text “Guess!” in it.
 
-const button = document.querySelector("button");
-button.addEventListener("click", function () {
-
+const guessLetterButton = document.querySelector(".guess");
+guessLetterButton.addEventListener("click", function (e) {
+    
 });
 
 
 //2c The text input where the player will guess a letter.
 
-const inputBox = document.querySelector(".letter");
+const letterInput = document.querySelector(".letter");
 
 
 //2d The empty paragraph where the word in progress will appear.
 
-const buildWord = document.querySelector(".word-in-progress");
-buildWord.innerText.h
+const wordInProgress = document.querySelector(".word-in-progress");
+wordInProgress.innerText = placeholderLetters.join("");
+placeholder(word);
 
 
 //2e The paragraph where the remaining guesses will display.
 
-const pgGuessRem = document.querySelector(".remaining");
+const remainingGuessesElement = document.querySelector(".remaining");
 
 
 //2f The span inside the paragraph where the remaining guesses will display.
 
-const spanGuessRem = document.querySelector(".remaining span");
+const remainingGuessesSpan = document.querySelector(".remaining span");
 
 
 //2g The empty paragraph where messages will appear when the player guesses a letter.
 
-const guessReply = document.querySelector(".message");
+const message = document.querySelector(".message");
 
 
 
 //2h The hidden button that will appear prompting the player to play again.
 
-const hiddenButton = document.querySelector(".play-again hide")
-hiddenButton.addEventListener("click", function () {
+const playAgainButton = document.querySelector(".play-again")
+playAgainButton.addEventListener("click", function () {
 
 });
 
 //3 Create another global variable called word and give it the value of "magnolia".
 //Magnolia is the starting word to test out the game until you fetch words from a hosted file in a later setup.
 
-const word = `magnolia`;
+const word = "magnolia";
 
 
 
