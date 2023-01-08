d1b49a234cfa994bb622735f8b29ebeee7db75d9
Madie Few <mfew.business@gmail.com>
2023-01-07 19:30:25 -0600

    Solution code corrections part two

--- js/script.js
+++ js/script.js
@@ -9,11 +9,8 @@ const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again")
 
 const word = "magnolia";
+const guessedLetters = [];
 
-playAgainButton.addEventListener("click", function (e) {
-    e.preventDefault();
-
-});
 
 const placeholder = function (word){
     const placeholderLetters = [];
@@ -28,7 +25,37 @@ placeholder(word);
 
 guessLetterButton.addEventListener("click", function (e) {
     e.preventDefault();
+    message.innerText = "";
     const guess = letterInput.value;
-    console.log(guess);
+    const goodGuess = validateInput(guess);
+
+    if (goodGuess) {
+        makeGuess(guess);
+    }
+
     letterInput.value = "";
 });
+
+
+const validateInput = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0) {
+        message.innerText = "Please enter a letter.";
+    } else if (input.length > 1) {
+        message.innerText = "Please enter a single letter.";
+    } else if (!input.match(acceptedLetter)) {
+        message.innerText = "Please enter a letter from A to Z.";
+    } else {
+        return input;
+    }
+};
+
+const makeGuess = function (guess) {
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes(guess)) {
+        message.innerText = "You already guessed that letter! Try again!"
+    } else {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+    }
+};
