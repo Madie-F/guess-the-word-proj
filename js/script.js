d7937db99229cd48327f0e5ae4194a086aa84d76
Madie Few <mfew.business@gmail.com>
2023-01-17 18:29:10 -0600

    Catching up, backing up

--- js/script.js
+++ js/script.js
@@ -12,13 +12,13 @@ const word = "magnolia";
 const guessedLetters = [];
 
 
-const placeholder = function (word){
-    const placeholderLetters = [];
-    for (const letter of word){
-        console.log(letter);
-        placeholderLetters.push("●");
-    }
-    wordInProgress.innerText = placeholderLetters.join("");
+const placeholder = function (word) {
+  const placeholderLetters = [];
+  for (const letter of word) {
+    console.log(letter);
+    placeholderLetters.push("●");
+  }
+  wordInProgress.innerText = placeholderLetters.join("");
 };
 
 placeholder(word);
@@ -28,11 +28,10 @@ guessLetterButton.addEventListener("click", function (e) {
     message.innerText = "";
     const guess = letterInput.value;
     const goodGuess = validateInput(guess);
-
+  
     if (goodGuess) {
-        makeGuess(guess);
+      makeGuess(guess);
     }
-
     letterInput.value = "";
 });
 
@@ -40,22 +39,48 @@ guessLetterButton.addEventListener("click", function (e) {
 const validateInput = function (input) {
     const acceptedLetter = /[a-zA-Z]/;
     if (input.length === 0) {
-        message.innerText = "Please enter a letter.";
+      message.innerText = "Please enter a letter.";
     } else if (input.length > 1) {
-        message.innerText = "Please enter a single letter.";
+      message.innerText = "Please enter a single letter.";
     } else if (!input.match(acceptedLetter)) {
-        message.innerText = "Please enter a letter from A to Z.";
+      message.innerText = "Please enter a letter from A to Z.";
     } else {
-        return input;
+      return input;
     }
 };
 
 const makeGuess = function (guess) {
     guess = guess.toUpperCase();
     if (guessedLetters.includes(guess)) {
-        message.innerText = "You already guessed that letter! Try again!"
+      message.innerText = "You already guessed that letter, silly. Try again.";
     } else {
-        guessedLetters.push(guess);
-        console.log(guessedLetters);
+      guessedLetters.push(guess);
+      console.log(guessedLetters);
+      showGuessedLetters();
     }
+
 };
+
+const showGuessedLetters = function () {
+  guessedLettersElement.innerHTML = "";
+  for (const letter of guessedLetters) {
+    const li = document.createElement("li");
+    li.innerText = letter;
+    guessedLettersElement.append(li)
+  }
+}
+
+const updateWordInProgress = function (guessedLetters) {
+  const wordUpper = word.toUpperCase;
+  const wordArray = wordUpper.split("");
+  const revealWord = [];
+  for (const letter of wordArray) {
+    if (guessedLetters.includes(letter)) {
+      revealWord.push(letter.toUpperCase());
+    } else {
+      revealWord.push("●");
+    }
+  }
+}
+console.log(revealWord)
+
